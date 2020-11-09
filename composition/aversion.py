6a9e6a09a8f4f2005edbb8fb4f6f362ae21d5eb3
Rodrigo Valla <rodrigovalla@protonmail.ch>
2020-11-09 20:59:24 -0300

    Uploading official g4g versions...

--- composition/aversion.py
+++ composition/aversion.py
@@ -1,4 +1,4 @@
 #Triggering a new infectoDoppler version
 from infectodoppler import infectoDoppler
 
-i = infectoDoppler("versions/g4g_2020_Basal/g4g_2020_Basal.json")
+i = infectoDoppler("versions/g4g_2020_2ndWave2/g4g_2020_2ndWave2.json")
